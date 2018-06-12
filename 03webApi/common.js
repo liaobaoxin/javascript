dd910188d1c7dfaa8b06bb9869ff43fd359c06ef
lbx <837944603@qq.com>
2018-06-12 15:20:32 +0800

    04案例切换背景图片.html

--- 03webApi/common.js
+++ 03webApi/common.js
@@ -19,3 +19,29 @@ function getInnerText(element) {
         return element.textContent;
     }
 }
+
+
+//获取任意一个父级元素的第一个子级元素
+function getFirstElementChild(element) {
+    if(element.firstElementChild){//true--->支持
+        return element.firstElementChild;
+    }else{
+        var node=element.firstChild;//第一个节点
+        while (node&&node.nodeType!=1){
+            node=node.nextSibling;
+        }
+        return node;
+    }
+}
+//获取任意一个父级元素的最后一个子级元素
+function getLastElementChild(element) {
+    if(element.lastElementChild){//true--->支持
+        return element.lastElementChild;
+    }else{
+        var node=element.lastChild;//第一个节点
+        while (node&&node.nodeType!=1){
+            node=node.previousSibling;
+        }
+        return node;
+    }
+}
