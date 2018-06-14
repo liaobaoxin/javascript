0ee3137615790eec9e29dbcdbcf7c07ebb2d6983
xxx <837944603@qq.com>
2018-06-14 21:00:33 +0800

    15美女时钟.html

---
+++ 04webApi/common.js
@@ -0,0 +1,70 @@
+function my$(id) {
+    return document.getElementById(id);
+}
+//设置任意的标签中间的任意文本内容
+function setInnerText(element,text) {
+    //判断浏览器是否支持这个属性
+    if(typeof element.textContent =="undefined"){//不支持
+        element.innerText=text;
+    }else{//支持这个属性
+        element.textContent=text;
+    }
+}
+
+//获取任意标签中间的文本内容
+function getInnerText(element) {
+    if(typeof element.textContent=="undefined"){
+        return element.innerText;
+    }else{
+        return element.textContent;
+    }
+}
+
+
+//获取任意一个父级元素的第一个子级元素
+function getFirstElementChild(element) {
+    if(element.firstElementChild){//true--->支持
+        return element.firstElementChild;
+    }else{
+        var node=element.firstChild;//第一个节点
+        while (node&&node.nodeType!=1){
+            node=node.nextSibling;
+        }
+        return node;
+    }
+}
+//获取任意一个父级元素的最后一个子级元素
+function getLastElementChild(element) {
+    if(element.lastElementChild){//true--->支持
+        return element.lastElementChild;
+    }else{
+        var node=element.lastChild;//第一个节点
+        while (node&&node.nodeType!=1){
+            node=node.previousSibling;
+        }
+        return node;
+    }
+}
+
+//添加事件兼容代码
+function addEventListener(element,type,fnName) {
+    if(element.addEventListener){
+        element.addEventListener(type,fnName,false);
+    }else if(element.attachEvent){
+        element.attachEvent("on"+type,element)
+    }else {
+        element["on"+type]=fnName;
+    }
+}
+
+
+//解绑事件兼容代码
+function removeEventListener(element,type,fnName) {
+    if(element.removeEventListener){
+        element.removeEventListener(type,fnName,false);
+    }else if(element.detachEvent){
+        element.detachEvent("on"+type,element)
+    }else {
+        element["on"+type]=null;
+    }
+}
