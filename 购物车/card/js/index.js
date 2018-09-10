e333eb225fc391d190eb49047b3656fa02dbc959
xxx <837944603@qq.com>
2018-09-10 21:02:10 +0800

    1

--- 购物车/card/js/index.js
+++ 购物车/card/js/index.js
@@ -2,10 +2,19 @@ $(function () {
     $(".container").fullpage({
         sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
         verticalCentered: false,
-        navigation:true,
-        afterLoad:function (link,index) {
-            $(".section").eq(index-1).addClass("now");
-
+        navigation: true,
+        afterLoad: function (link, index) {
+            $(".section").eq(index - 1).addClass("now");
+        },
+        afterRender: function () {
+            $(".more").click("on", function () {
+                $.fn.fullpage.moveSectionDown();
+            });
+        },
+        onLeave: function (index, nextInt, direction) {
+            if (index == 2 && nextInt == 3) {
+                $(".section").eq(index - 1).addClass("leave");
+            }
         }
     })
 });
