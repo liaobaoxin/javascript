8c7337d9b69779384e3b08880b376e9da02576a1
xxx <837944603@qq.com>
2018-09-12 21:12:11 +0800

    购物车\card

--- 购物车/card/js/index.js
+++ 购物车/card/js/index.js
@@ -10,11 +10,22 @@ $(function () {
             $(".more").click("on", function () {
                 $.fn.fullpage.moveSectionDown();
             });
+            $(".section.screen04  .sofa").on('transitionend',function () {
+                console.log("11");
+            });
+
         },
         onLeave: function (index, nextInt, direction) {
             if (index == 2 && nextInt == 3) {
                 $(".section").eq(index - 1).addClass("leave");
+                console.log(index);
+            }else if(index == 3 && nextInt == 4){
+                $(".section").eq(index - 1).addClass("leave");
             }
-        }
+        },
+
+
+        scrollingSpeed:1000
+
     })
 });
