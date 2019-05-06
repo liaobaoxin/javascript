53158dfc5bfdd8159a5ff03122ab55129e8e3abc
lbx <837944603@qq.com>
2019-05-06 17:40:34 +0800

    webpack

---
+++ vue/webpack-demo/src/main.js
@@ -0,0 +1,4 @@
+import $ from "jquery";
+$(function () {
+  $("h1").css('backgroundColor','yellow')
+});
---
+++ vue/webpack-demo/webpack.config.js
@@ -0,0 +1,9 @@
+const filePath = require('path');
+
+module.exports = {
+    entry: __dirname+"/src/main.js",
+    output: {
+        path: __dirname+ "/dist",
+        filename: "bundle.js"
+    }
+}
