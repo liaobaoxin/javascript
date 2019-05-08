3ec20d2b548eaedcc169780cdb21bf61efb06ea9
lbx <837944603@qq.com>
2019-05-08 17:55:57 +0800

    vue-cms

--- vue/vue-cms/src/main.js
+++ vue/vue-cms/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
 import './lib/css/mui.css'
+import './lib/css/icons-extra.css'
+
 import { Header } from 'mint-ui';
 Vue.component(Header.name, Header);
+
+
+import router from "./router.js"
 
 import app from './app.vue'
 
@@ -9,6 +16,7 @@ import app from './app.vue'
 import 'mint-ui/lib/style.css'
 new Vue({
     el:"#app",
+    router,
     render:c=>c(app)
-})
+});
 
