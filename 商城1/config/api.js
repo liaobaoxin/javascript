748f7f8d35c64760dba9dc2eba70c976f5842f16
xxx <837944603@qq.com>
2019-05-18 23:33:34 +0800

    团购

---
+++ 商城1/config/api.js
@@ -0,0 +1,7 @@
+var WxApiRoot="http://localhost:8080/wx/";
+
+
+module.exports={
+    GoodsCount: WxApiRoot + 'goods/count', //统计商品总数
+    IndexUrl: WxApiRoot + 'home/index' //首页数据接口
+}
--- 商城1/utils/util.js
+++ 商城1/utils/util.js
@@ -1,19 +1,30 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+/**
+ * 封封微信的的request
+ */
+function request(url, data = {}, method = "GET") {
+  return new Promise((resolve, reject)=> {
+    wx.request({
+      url: url,
+      data: data,
+      method: method,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        if (res.statusCode == 200) {
+          resolve(res.data);
+        } else {
+          reject(res.errMsg);
+        }
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+      },
+      fail: function(err) {
+        reject(err)
+      }
+    })
+  });
 }
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
-
-module.exports = {
-  formatTime: formatTime
+module.exports={
+  request
 }
