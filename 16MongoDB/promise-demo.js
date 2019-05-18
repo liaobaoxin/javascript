8d02ca8a826ddfbce25b27ee66b835d2172f0b1a
xxx <837944603@qq.com>
2019-05-18 09:43:09 +0800

    测试

---
+++ 16MongoDB/promise-demo.js
@@ -0,0 +1,10 @@
+console.log(1);
+var fs = require('fs');
+fs.readFile('./data.txt','utf8',function (err,data) {
+    console.log(data);
+
+});
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
