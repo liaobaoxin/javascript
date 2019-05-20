899bb5cf4a22468cee6685cd32dca9c2312aea37
lbx <837944603@qq.com>
2019-05-20 17:58:35 +0800

    引用vant组件

--- 商城1/config/api.js
+++ 商城1/config/api.js
@@ -3,5 +3,6 @@ var WxApiRoot="http://localhost:8080/wx/";
 
 module.exports={
     GoodsCount: WxApiRoot + 'goods/count', //统计商品总数
-    IndexUrl: WxApiRoot + 'home/index' //首页数据接口
+    IndexUrl: WxApiRoot + 'home/index', //首页数据接口
+    SearchIndex: WxApiRoot + 'search/index' //搜索关键字
 }
